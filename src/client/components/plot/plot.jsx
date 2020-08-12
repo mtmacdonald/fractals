14a2d6a57c13e295d2a9303f84247e2068d05ce5
Mark Macdonald <mark.t.macdonald@googlemail.com>
2020-08-12 16:45:18 +0200

    Render all points (slow).

--- src/client/components/plot/plot.jsx
+++ src/client/components/plot/plot.jsx
@@ -1,11 +1,38 @@
 import React from "react";
 import { Stage } from "@inlet/react-pixi";
 import Point from "./point";
+const randomColor = () =>
+  `0x${((Math.random() * 0xffffff) << 0).toString(16).padStart(6, "0")}`;
+
+const points = Array(500)
+  .fill(undefined)
+  .map((_, x) => {
+    return Array(500)
+      .fill(undefined)
+      .map((_, y) => {
+        return {
+          x,
+          y,
+          color: randomColor(),
+        };
+      });
+  });
 
 export const Plot = ({}) => {
   return (
     <Stage width={400} height={400}>
-      <Point x={10} y={10} width={1} height={1} fill={0xff0000} />
+      {points.map((r, ix) =>
+        r.map((p, iy) => (
+          <Point
+            key={`${ix}_${iy}`}
+            x={p.x}
+            y={p.y}
+            width={1}
+            height={1}
+            fill={p.color}
+          />
+        ))
+      )}
     </Stage>
   );
 };
